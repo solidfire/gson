cbbdeaa0c6e7dc28804ab4fd9deb024f9e42feed
Jason Ryan Womack <jason.womack@solidfire.com>
2016-09-28 15:53:16 -0600

    Refactored namespace to com.{google=>solidfire}

---
+++ gson/src/main/java/com/solidfire/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link com.solidfire.gson.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.solidfire.gson.annotations;
